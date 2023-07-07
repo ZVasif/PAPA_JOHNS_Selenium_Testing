a479335d3637095faf1134e0abbc39bad5a5ef80
master <vasif_zv@bk.ru>
2023-07-07 23:42:32 +0400

    Dominos is starting

---
+++ src/Utils/BaseStaticDriver.java
@@ -0,0 +1,64 @@
+package Utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class BaseStaticDriver {
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    static
+    {
+        KalanOncekileriKapat();
+        Logger logger = Logger.getLogger("");
+        logger.setLevel(Level.SEVERE);
+
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY,"true");
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+
+        wait=new WebDriverWait(driver,Duration.ofSeconds(30));
+
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    }
+
+     public static void closeWindow()
+     {
+         try {
+             Thread.sleep(3000);
+         } catch (InterruptedException e) {
+             throw new RuntimeException(e);
+         }
+         driver.quit();
+     }
+
+    public static void waitLocator(int saniye)
+    {
+
+        try {
+            Thread.sleep(saniye*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void KalanOncekileriKapat() {
+
+        try {
+            Runtime.getRuntime().exec("taskkill /F /IM chromedriver.exe /T");
+        } catch (Exception ignored) {
+
+        }
+    }
+
+}
---
+++ src/_01.java
@@ -0,0 +1,11 @@
+import Utils.BaseStaticDriver;
+
+public class _01 extends BaseStaticDriver {
+    public static void main(String[] args){
+        driver.get("https://www.dominos.com");
+
+
+        closeWindow();
+
+    }
+}
