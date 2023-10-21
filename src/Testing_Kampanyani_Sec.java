dd5304bd3f0695b5087b680f3cd5bf5fa3b130a3
master <vasif_zv@bk.ru>
2023-10-21 23:22:05 +0400

    Kampaniya Sec first step is finished.

--- src/Testing_Kampanyani_Sec.java
+++ src/Testing_Kampanyani_Sec.java
@@ -2,6 +2,8 @@ import Utils.BaseStaticDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public class Testing_Kampanyani_Sec extends BaseStaticDriver {
     public static void main(String[] args) {
 
@@ -15,5 +17,21 @@ public class Testing_Kampanyani_Sec extends BaseStaticDriver {
         dahasonra2.click();
         WebElement kamSec= driver.findElement(By.cssSelector("[alt=\"Restoran Seç\"]"));
         kamSec.click();
+        List<WebElement> ozelKamp=driver.findElements(By.cssSelector("[id^='9afebc804b'] [class='row']"));
+
+        for (WebElement kampaniya:ozelKamp){
+            System.out.println(kampaniya.getText()+"\n***************");
+        }
+
+//        int mehsulSayi=ozelKamp.size();
+//        System.out.println("mehsulSayi = " + mehsulSayi);
+
+        if (ozelKamp.size()>3){
+            System.out.println((ozelKamp.size()-3)+" ədəd yeni məhsul əlavə olunub.");
+        } else if (ozelKamp.size()<3) {
+            System.out.println((ozelKamp.size()-3)+" ədəd məhsul kampaniyadan çıxarılıb.");
+        }else System.out.println("Kampaniyaya yeni məhsul əlavə olunmayıb.");
+
+        closeWindow();
     }
 }
